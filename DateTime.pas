61ef17856cd6de5a7f10c2a153c7872664b8ed91
Sasha Tkachenko <aletka@sfedu.ru>
2021-10-21 09:23:17 +0300

    add func LaterInYear

--- DateTime.pas
+++ DateTime.pas
@@ -1,5 +1,8 @@
 ﻿function IsLeapYear(x : integer):= ((x mod 4 = 0) and (x mod 100 > 0)) or (x mod 400 = 0);
-
+function LaterInYear(day1, day2 :DateTime) : DateTime;
+begin
+  result := max(day1,day2);
+end;
 begin
   
 end.
